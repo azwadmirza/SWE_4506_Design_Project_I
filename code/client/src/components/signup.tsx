f020c7b98a043da27c4aaef144eac66dca6e4af3
Mirza Mohammad Azwad <50703605+mirzaazwad@users.noreply.github.com>
2023-10-22 03:18:47 +0600

    Login Without SMTP

--- code/client/src/components/signup.tsx
+++ code/client/src/components/signup.tsx
@@ -11,14 +11,12 @@ import {
 import DOMPurify from "dompurify";
 import { useSignUp } from "../hooks/useSignUp";
 import "../assets/css/signup.css";
+import { LoadingProps } from "./login";
 
-interface SignUpProps {
-  changeState: React.Dispatch<React.SetStateAction<string>>;
-}
 
-const SignUp = ({ changeState }: SignUpProps) => {
+
+const SignUp = ({changeLoadingState}:LoadingProps) => {
   const {
-    loading, 
     error,
     username,
     setUsername,
@@ -30,9 +28,8 @@ const SignUp = ({ changeState }: SignUpProps) => {
     changeConfirmPassword,
     errorPassword,
     errorConfirmPassword,
-    dsiable,
     signup
-  } = useSignUp();
+  } = useSignUp(changeLoadingState);
   const [passwordVisibility, setPasswordVisibility] = useState<string>(
     "password"
   );
@@ -40,7 +37,8 @@ const SignUp = ({ changeState }: SignUpProps) => {
     string
   >("password");
 
-  const handleSubmit = () => {
+  const handleSubmit = (e:React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
     signup();
   };
 
@@ -48,7 +46,7 @@ const SignUp = ({ changeState }: SignUpProps) => {
     <Card className="signup">
       <Card.Body>
         <h4>Sign Up</h4>
-        <Form onSubmit={handleSubmit}>
+        <Form onSubmit={(e) => handleSubmit(e)}>
           <div className="errorBox">{error}</div>
 
           <div className="inputbox">
@@ -143,7 +141,7 @@ const SignUp = ({ changeState }: SignUpProps) => {
             name="submit"
             value="submit"
             disabled={
-              loading || dsiable
+              errorPassword !== "" || errorConfirmPassword !== ""
             }
             id="buttonRegister"
           >
@@ -157,7 +155,7 @@ const SignUp = ({ changeState }: SignUpProps) => {
         <div className="login-link">
           <p>
             Already have an account?{" "}
-            <a onClick={() => changeState("Login")} className="link-to-login">Login</a>
+            <a href="/login" className="link-to-login">Login</a>
           </p>
         </div>
       </Card.Body>
--- code/server/gateway/serializers.py
+++ code/server/gateway/serializers.py
@@ -1,15 +1,18 @@
 from rest_framework import serializers
 from .models import user
+from django.contrib.auth.hashers import make_password
 
 class userSerializer(serializers.ModelSerializer):
     class Meta:
         model = user
-        fields = ('id', 'username','password', 'email', 'date_joined', 'is_active', 'is_staff')
+        fields = ('id', 'username','password', 'email', 'date_joined', 'is_active','verified')
 
     def create(self, validated_data):
         """
         Create and return a new User instance, given the validated data.
         """
+        # Hash the password before saving it
+        validated_data['password'] = make_password(validated_data['password'])
         return user.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
@@ -18,8 +21,9 @@ class userSerializer(serializers.ModelSerializer):
         """
         instance.username = validated_data.get('username', instance.username)
         instance.email = validated_data.get('email', instance.email)
-        instance.password = validated_data.get('password', instance.password)
+        if 'password' in validated_data:
+            instance.password = make_password(validated_data['password'])
         instance.is_active = validated_data.get('is_active', instance.is_active)
-        instance.is_staff = validated_data.get('is_staff', instance.is_staff)
+        instance.verified = validated_data.get('verified', instance.verified)
         instance.save()
         return instance
