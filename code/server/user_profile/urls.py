3ab02336ebf5e741bd9e4bce98fa3e616bee1e93
Mirza Mohammad Azwad <50703605+mirzaazwad@users.noreply.github.com>
2023-10-22 14:44:51 +0600

    Profile Without Profile Picture

--- code/server/user_profile/urls.py
+++ code/server/user_profile/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import Profile
 
 urlpatterns = [
-    path('profile/', Profile.as_view(), name='profile'),
+    path('get-profile/<int:id>/', Profile.as_view(), name='profile', kwargs={'http_method_names': ['get']}),
 ]
--- code/server/user_profile/views.py
+++ code/server/user_profile/views.py
@@ -1,17 +1,16 @@
-
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.decorators import authentication_classes, permission_classes
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.decorators import authentication_classes
 from rest_framework import status
 from gateway.models import user
+from django.contrib.auth.decorators import login_required
 
-@authentication_classes([TokenAuthentication])
-@permission_classes([IsAuthenticated])
+@authentication_classes([JWTAuthentication])
 class Profile(APIView):
-    def get(self, request):
-        id = request.query_params.get('id')
+    permission_classes = []
+    def get(self, request, *args, **kwargs):
+        id = kwargs.get('id')
         if id is None:
             return Response({'message': 'id parameter is required'}, status=status.HTTP_400_BAD_REQUEST)
 
@@ -28,3 +27,34 @@ class Profile(APIView):
                 return Response({'message': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
         except user.DoesNotExist:
             return Response({'message': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
+        
+    def put(self, request, *args, **kwargs):
+        id = request.data.get('id')  # Assuming you pass the user's ID in the request data
+        if id is None:
+            return Response({'message': 'id parameter is required'}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            user_instance = user.objects.get(id=id)
+            user_data = request.data  # This assumes you are sending updated user data in the request
+
+            # Update user fields
+            if 'username' in user_data:
+                user_instance.username = user_data['username']
+            if 'email' in user_data:
+                user_instance.email = user_data['email']
+            if 'image' in user_data:
+                user_instance.image = user_data['image']
+
+            user_instance.save()
+
+            response_data = {
+                'id': user_instance.id,
+                'username': user_instance.username,
+                'email': user_instance.email,
+                'image': user_instance.image
+            }
+
+            return Response(response_data)
+        except user.DoesNotExist:
+            return Response({'message': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
+        
