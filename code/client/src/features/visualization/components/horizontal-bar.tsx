38aacb21a5143ce22cae8e40b2d912ec76e8e6ea
Mirza Mohammad Azwad <50703605+mirzaazwad@users.noreply.github.com>
2024-01-10 04:06:33 +0600

    Visualization #1

---
+++ code/client/src/features/visualization/components/horizontal-bar.tsx
@@ -0,0 +1,50 @@
+import { Bar } from "react-chartjs-2";
+
+interface IBarPlotProps{
+    chartData: {
+        labels: string[];
+        datasets: {
+            label: string;
+            data: string[];
+            borderColor: string;
+            borderWidth: number;
+            backgroundColor: string[];
+        }[];
+    }
+}
+
+const HorizontalBarPlot = ({chartData}:IBarPlotProps) => {
+  const isValidData = chartData.datasets.every((dataset) =>
+    dataset.data.every((value) => !isNaN(parseFloat(value)))
+  );
+  if(isValidData){
+    return ( 
+      <>
+      <Bar
+      data={chartData}
+      options={{
+          indexAxis:  "y",
+          responsive: true,
+          plugins: {
+            legend: {
+              position: "top"
+            },
+            title: {
+              display: true,
+              text: "Horizontal Bar Plot"
+            }
+          }
+        }}/>
+      </>
+   );
+  }
+  else{
+    return (
+      <div className="class-chart">
+        <p style={{color:"red"}}>Invalid Data For Horizontal Bar Plot: Requires Numeric Values for Dependent Variable</p>
+      </div>
+    )
+  }
+}
+ 
+export default HorizontalBarPlot;
