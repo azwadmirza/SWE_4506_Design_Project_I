43d77b6c2f39965ccda5b33c9c68d7745f50baa5
Mirza Mohammad Azwad <50703605+mirzaazwad@users.noreply.github.com>
2024-01-15 03:52:53 +0600

    XGBoost

--- code/server/xgBoost/urls.py
+++ code/server/xgBoost/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-     path('start/', views.xgboost_model.as_view(), name='xgboost'),
+     path('classification/', views.xgboost_classification_model.as_view(), name='xgboost'),
+     path('regression/', views.xgboost_regression_model.as_view(), name='xgboost'),
 ]
